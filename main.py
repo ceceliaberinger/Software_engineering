a2a634fc383291dfac72b6c5a23a5671318df1ac
Cecelia <ceceliaberinger@gmail.com>
2023-03-13 01:49:40 -0400

    first commit

--- main.py
+++ main.py
@@ -24,4 +24,32 @@ def encode(password):
 
 
 
-print(encode("12345555"))
+if __name__ == '__main__':
+
+    program = True
+
+while program == True:
+
+    print('Menu')
+    print('-------------')
+    print('1.Encode')
+    print('2.Decode')
+    print('3.Quit')
+
+    select = int(input('Please enter an option: '))
+
+
+
+
+    if select == 1:
+        password = input('Please enter your password to encode: ')
+        encoded_password = encode(password)
+        print('Your password has been encoded and stored!')
+
+
+    if select == 2:
+        print('The encoded password is', encoded_password,', and the original password is', decode(encoded_password) )
+
+
+    if select == 3:
+        program = False
